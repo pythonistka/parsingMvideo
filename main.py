fee22eb6c547404a0dfbba9b84db2e85d10f136d
Lopatina Darya <baboleh@rambler.ru>
2022-08-02 00:40:31 +0300

    Прописали отдельную функию для сохранения всех данных о товаре в одном файле(включая базовую цену, цену со скидкой и кол-ом бонусов)

--- main.py
+++ main.py
@@ -166,9 +166,10 @@ def get_data():
     # Cобираем цены скидки и бонусы на товар
     # (с сайта м-видео prices запрос, копируем cURL(bush), на сайте
     # https://curlconverter.com/#python конвертируем cURL запрос для Python, копируем с params и запрос
-    # до params склеем список с Ids в строку с помощью метода join
 
+    # до params склеем список с Ids в строку с помощью метода join
     products_ids_str = ','.join(products_ids)
+
     # подставляем строку products_ids_str в params
     params = {
         'productIds': products_ids_str,
@@ -184,11 +185,70 @@ def get_data():
     with open('3_prices.json', 'w') as file:
         json.dump(response, file, indent=4, ensure_ascii=False)
 
+    # создаем словарь под наши данные
+    items_prices = {}
+
+    # получаем список со словарями в котором хранятся объекты с данными
+    material_prices = response.get('body').get('materialPrices')
+
+    # пробегаемся по нему циклом
+    for item in material_prices:
+        item_id = item.get('price').get('productId')
+        item_base_price = item.get('price').get('basePrice')
+        item_sale_price = item.get('price').get('salePrice')
+        item_bonus = item.get('bonusRubles').get('total')
+
+        # на каждой итерации записываем в словарь новую пару, ключом будет ID товара, а значением словарь с данными в
+        # виде базового прайса, прайса со скидкой и бонусами
+        items_prices[item_id] = {
+            'item_basePrice': item_base_price,
+            'item_salePrice': item_sale_price,
+            'item_Bonus': item_bonus
+        }
+
+        # после выходим из цикла и записываем данные в json
+        with open('4_items_prices.json', 'w') as file:
+            json.dump(items_prices, file, indent=4, ensure_ascii=False)
+
+
+
+# объединение словарей с данными выносим в отдельныую функцию
+def get_result():
+    # читаем словарь с данными по планшетам
+    with open('2_items.json') as file:
+        products_data = json.load(file)
+
+    # и следом словарь с прайсами
+    with open('4_items_prices.json') as file:
+        products_prices = json.load(file)
+
+    # забираем из словаря с данными только данные по планшетам
+    products_data = products_data.get('body').get('products')
+
+    # пробегаемся циклом по списку словарей и первым делом получаем id продукта
+    for item in products_data:
+        product_id = item.get('productId')
+
+        # далее нужно найти совпадения в словаре с прайсами и если id там существует, сохраняем словарь с данными по
+        # базовой цене, цене со скидкой и бонусами
+        if product_id in products_prices:
+            prices = products_prices[product_id]
+
+        # создаем новые пары в итоговом словаре с данными
+        item['item_basePrice'] = prices.get('item_basePrice')
+        item['item_salePrice'] = prices.get('item_salePrice')
+        item['item_Bonus'] = prices.get('item_Bonus')
+
+    # выходим из цикла и записываем данные в json
+    with open('5_result.json', 'w') as file:
+        json.dump(products_data, file, indent=4, ensure_ascii=False)
+
 
 
 
 def main():
     get_data()
+    get_result()
 
 if __name__ == '__main__':
     main()
